3e7516082cec2144455aff7f16d539f8ad374689
Michał Wdowik <michal.wdowik@nsp.biz.pl>
2023-10-25 17:14:15 +0200

    Improve styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { LazyComponent } from './lazy/lazy.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'courses', component: CoursesComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'lazy', component: LazyComponent },
   { path: '**', redirectTo: '/home' },
 ];
 
