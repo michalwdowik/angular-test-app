b1d4043a171cc8c92a9fe0937dcae02eb0643981
Michał Wdowik <michal.wdowik@nsp.biz.pl>
2023-10-31 12:51:13 +0100

    Add login status to localstorage

--- src/app/shared/services/login/login.service.ts
+++ src/app/shared/services/login/login.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@angular/core';
 
+const STORAGE_KEY = 'logged';
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
   enableProtectedRoute = false;
 
+  constructor() {
+    const storedValue = localStorage.getItem(STORAGE_KEY);
+    if (storedValue !== null) {
+      this.enableProtectedRoute = JSON.parse(storedValue);
+    }
+  }
+
   toggleAccess() {
     this.enableProtectedRoute = !this.enableProtectedRoute;
+    localStorage.setItem(
+      STORAGE_KEY,
+      JSON.stringify(this.enableProtectedRoute)
+    );
   }
 }
