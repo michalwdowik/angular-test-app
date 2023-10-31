7765879396b44f23af9524cc3cb646347bd42ad9
Michał Wdowik <michal.wdowik@nsp.biz.pl>
2023-10-31 11:42:29 +0100

    Add theme state to local storage

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,18 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { ThemeService } from 'src/app/shared/services/theme/theme.service';
+
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
   @Output() readonly darkModeSwitched = new EventEmitter<boolean>();
+  isDarkTheme = false;
 
   constructor(private themeService: ThemeService) {}
+
+  ngOnInit() {
+    this.isDarkTheme = this.themeService.isDarkTheme(); // Initialize the toggle button state
+  }
+
   showSidenav = false;
 
   toggleTheme() {
     this.themeService.toggleTheme();
+    this.isDarkTheme = this.themeService.isDarkTheme(); // Update the toggle button state
   }
 
   onToggleSidenav() {
--- src/app/shared/services/theme/theme.service.ts
+++ src/app/shared/services/theme/theme.service.ts
@@ -4,20 +4,30 @@ import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
   providedIn: 'root',
 })
 export class ThemeService {
-  private currentTheme: 'light' | 'dark' = 'light';
   private renderer: Renderer2;
+  private currentTheme: 'light' | 'dark' = 'light';
 
   constructor(rendererFactory: RendererFactory2) {
     this.renderer = rendererFactory.createRenderer(null, null);
+    this.initializeTheme();
   }
 
   toggleTheme() {
-    this.currentTheme = this.currentTheme === 'light' ? 'dark' : 'light';
+    this.currentTheme = this.isDarkTheme() ? 'light' : 'dark';
+    localStorage.setItem('theme', this.currentTheme);
     this.updateTheme();
   }
 
   isDarkTheme() {
     return this.currentTheme === 'dark';
+  }
+
+  private initializeTheme() {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme === 'dark' || storedTheme === 'light') {
+      this.currentTheme = storedTheme as 'light' | 'dark';
+      this.updateTheme();
+    }
   }
 
   private updateTheme() {
